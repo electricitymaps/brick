9968693800b48252baea2e5ea27795e9d072c89a
skovhus <kenneth.skovhus@gmail.com>
2020-08-23 21:02:56 +0200

    Auto format code using Black

--- brick/logger.py
+++ brick/logger.py
@@ -4,6 +4,6 @@ import sys
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
