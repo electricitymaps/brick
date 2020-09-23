325db45f09643f6d2db501872ee23c44b5c8671e
skovhus <kenneth.skovhus@gmail.com>
2020-09-23 22:18:19 +0200

    Document that expand_brick_environment_variables works

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,5 +1,40 @@
 import pytest
 
+from brick.lib import expand_brick_environment_variables
 
-def test_hello_world():
-    assert 1 == 1  # py.test needs at least one test!
+
+def test_expand_brick_environment_variables(monkeypatch):
+    assert expand_brick_environment_variables("") == ""
+
+    # Supports default if BRICK_ variable is not found
+    assert (
+        expand_brick_environment_variables("tag: server:${BRICK_COMMIT_SHA:-latest}")
+        == "tag: server:latest"
+    )
+
+    # Should expand BRICK_ variables if found
+    monkeypatch.setenv("BRICK_COMMIT_SHA", "1234")
+    assert (
+        expand_brick_environment_variables("tag: server:${BRICK_COMMIT_SHA:-latest}")
+        == "tag: server:1234"
+    )
+
+    assert (
+        expand_brick_environment_variables("tag: server:${BRICK_COMMIT_SHA}") == "tag: server:1234"
+    )
+
+    # Should not expand non BRICK_ prefixed variables
+    assert (
+        expand_brick_environment_variables("tag: server:${COMMIT_SHA:-latest}")
+        == "tag: server:${COMMIT_SHA:-latest}"
+    )
+
+    # Raises exception if unsupported format is used
+    with pytest.raises(AssertionError) as excinfo:
+        expand_brick_environment_variables("tag: server:$BRICK_FOO")
+    assert "faulty BRICK_ environment" in str(excinfo.value)
+
+    # Raises exception if no default value is provided
+    with pytest.raises(AssertionError) as excinfo:
+        expand_brick_environment_variables("tag: server:${BRICK_FOO}")
+    assert "not find environment variable BRICK_FOO or default value" in str(excinfo.value)
