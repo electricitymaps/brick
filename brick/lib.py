618386b84d1033f48705a25a1f5b8cdd7231c85e
Olivier Corradi <olivier.corradi@gmail.com>
2020-12-28 09:53:24 +0100

    Lint

--- brick/lib.py
+++ brick/lib.py
@@ -141,19 +141,17 @@ def get_config(target):
             expanded_config = expand_brick_environment_variables(raw_config)
             obj = yaml.load(expanded_config, Loader=yaml.FullLoader)
             # Expand environment variables when needed using pattern ${var}
-            pattern = re.compile('.*?\${(\w+)}.*?')
-            for (_, step) in obj.get('steps', {}).items():
-                for (k, value) in step.get('environment', {}).items():
+            pattern = re.compile(".*?\${(\w+)}.*?")
+            for (_, step) in obj.get("steps", {}).items():
+                for (k, value) in step.get("environment", {}).items():
                     match = pattern.findall(value)  # to find all env variables
                     if match:
                         for g in match:
                             if g not in os.environ:
-                                raise Exception(
-                                    f'Missing environment variable "${g}"')
-                            value = value.replace(
-                                f'${{{g}}}', os.environ.get(g, g))
+                                raise Exception(f'Missing environment variable "${g}"')
+                            value = value.replace(f"${{{g}}}", os.environ.get(g, g))
                         # assign
-                        step['environment'][k] = value
+                        step["environment"][k] = value
             return obj
     except FileNotFoundError:
         raise Exception(f"BUILD.yaml not found.")
