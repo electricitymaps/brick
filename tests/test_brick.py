72d17a74b7e68873ba588947e7156d36e042b651
Olivier Corradi <1655848+corradio@users.noreply.github.com>
2021-01-04 09:17:51 +0100

    Add chown (#77)
    * Add chown
    
    * Format
    
    * Adjust whitespaces and number of arguments in lint config
    
    * Formatted

--- tests/test_brick.py
+++ tests/test_brick.py
@@ -141,11 +141,11 @@ def test_examples_node_build_1_on_master(monkeypatch, caplog) -> None:
 
     assert info_logs == [
         "🔨 Preparing brick_example_node..",
-        "Cache invalidated by COPY [brick_example_node/package.json, "
-        "/home/brick_example...",
+        "Cache invalidated by COPY  [brick_example_node/package.json, "
+        "/home/brick_exampl...",
         "💯 Preparation phase done!",
         "🔨 Building brick_example_node..",
-        "Cache invalidated by COPY [brick_example_node/src, "
+        "Cache invalidated by COPY  [brick_example_node/src, "
         "/home/brick_example_node/src]",
         "💯 Finished building brick_example_node!",
     ]
@@ -273,7 +273,7 @@ def test_workspace_build(monkeypatch, caplog) -> None:
         "💯 Preparation phase done (cached)!",
         "🔨 Building brick_example_node..",
         "💯 Finished building brick_example_node (cached)!",
-        "Cache invalidated by COPY [brick_example_python/src, "
+        "Cache invalidated by COPY  [brick_example_python/src, "
         "/home/brick_example_python/src]",
         "💯 Finished building brick_example_python!",
     ]
