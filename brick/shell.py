7aab14e337478c76abe7e65ace92e9b255100458
skovhus <kenneth.skovhus@gmail.com>
2021-03-03 10:12:27 +0100

    Add command to lazy get the sha1 command

---
+++ brick/shell.py
@@ -0,0 +1,22 @@
+import subprocess
+
+from .logger import logger
+
+_sha1_command = None
+
+
+def run_shell_command(cmd: str, cwd: str = None) -> str:
+    return subprocess.check_output(cmd, shell=True, encoding="utf8", cwd=cwd).rstrip("\n")
+
+
+def get_sha1_command() -> str:
+    """Returns the sha1sum command support by the system"""
+    global _sha1_command
+    if not _sha1_command:
+        try:
+            run_shell_command("which sha1sum")
+            _sha1_command = "sha1sum"
+        except subprocess.CalledProcessError:
+            logger.info("sha1sum not found on this system (performance will be slightly degraded)")
+            _sha1_command = "shasum"
+    return _sha1_command
